cc242e6ec945d62b9de807a98c68a3def9942227
UnaGuo <gsj@heils.cn>
2019-12-02 11:05:11 +0800

    Update: modify the datasetbase class

--- datasets/datasets_base.py
+++ datasets/datasets_base.py
@@ -5,7 +5,6 @@
 #
 
 # -*- coding: utf-8 -*
-import logging
 from PIL import Image
 import torch
 import torch.utils.data as data
@@ -24,12 +23,15 @@ class DatasetsBase(data.Dataset):
 
         self.transform = transforms.Compose([])
 
+        self.pattern = []
+        self.target_w = 0
+        self.target_h = 0
+
 
     def setup(self):
         '''
         - When overriding: Read the file of the datasets, and load into the object
-        - :Alarm: Please call 'super' method at the end of the override method
-        :return: True/False means check pass or failed.
+        - Alarm: Please call 'super' method at the end of the override method
         '''
         if not isinstance(self.data, list):
             raise TypeError("DatasetsBase: 'self.data' should be 'list'.")
@@ -39,11 +41,20 @@ class DatasetsBase(data.Dataset):
         if not isinstance(self.transform, transforms.Compose):
             raise TypeError("DatasetsBase: 'self.train_transform''s values must be as type 'transforms.Compose'.")
 
+        if not isinstance(self.pattern, list):
+            raise TypeError("DatasetsBase: 'self.pattern' should be 'list'.")
+
+        if not isinstance(self.target_w, int) or not isinstance(self.target_h, int):
+            raise TypeError("DatasetsBase: 'self.target_w' & 'self.target_h' should be 'int'.")
 
         if len(self.data)*len(self.labels)==0:
             raise ValueError("DatasetsBase: variable(s) below may be null:"
                              "'self.data' is {}, "
                              "'self.labels' is {}.".format(self.data,self.labels))
+
+
+    def getinfo(self):
+        return self.pattern, self.target_w, self.target_h
 
 
     def __len__(self):
